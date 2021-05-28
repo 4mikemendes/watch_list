0a4c3706ff4f0f3001b5eb85e222604e2f116c30
이혜진 <4mikemendes@gmail.com>
2021-05-28 12:48:48 +0200

    creates a new List       assigns a newly created list as @list       redirects to the created list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,26 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
+  def show
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
